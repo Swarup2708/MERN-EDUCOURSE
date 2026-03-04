d4ec96933afec9abd81c6ab98800299fe7169203
Swarup2708 <chmcs2026.swarup.kumbhar@gmail.com>
2026-03-04 22:03:23 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   //baseURL: "http://localhost:3000/Material",
-  baseURL:'https://mern-educourse.onrender.com/Material'
+  baseURL:'https://mern-educourse.onrender.com'
 
 export default api;

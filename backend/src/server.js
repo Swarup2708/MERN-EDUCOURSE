12751b59198604cdac2af11b44f246656dd4e9d8
Swarup2708 <chmcs2026.swarup.kumbhar@gmail.com>
2026-03-05 16:21:07 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import {connectDB} from "./config/db.js";
 import materialRoutes from "./routes/materialRoutes.js";
-import n from "node:dns/promises"
 import cors from "cors";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001
-n.setServers(["1.1.1.1","8.8.8.8"
-    
-])
+
 
 app.use(cors(
     {
